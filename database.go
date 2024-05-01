5a1f5b8c5f3d0028596b118252b001391bf903d5
OmkarLande <omkarrlande.534@gmail.com>
2024-05-01 21:55:56 +0530

    initiate

---
+++ database.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+var db *mongo.Database
+
+func ConnectToDatabase(connectionString string) {
+	// Set client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	// Connect to MongoDB
+	var err error
+	client, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = client.Database("contacts")
+	println("Connected to MongoDB!")
+}
+
+// Define your data model here (e.g., Contact struct)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// Connect to MongoDB
+	connectionString := "mongodb+srv://admin:AGXdEDgYfmZcmLJt@cluster0.h1aicec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+	ConnectToDatabase(connectionString)
+}
