3258bf64c170ec048ff460c32ead38287e4cc580
OmkarLande <omkarrlande.534@gmail.com>
2024-05-01 21:57:17 +0530

    Refactor package name in database.go and import d package in main.go

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/OmkarLande/PRODIGY_SD_03/d"
+)
+
 func main() {
 	// Connect to MongoDB
 	connectionString := "mongodb+srv://admin:AGXdEDgYfmZcmLJt@cluster0.h1aicec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
